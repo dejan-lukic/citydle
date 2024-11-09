926645661e155132fe2e3308ef8336bbbc5f40dc
dejan-lukic <dejan.lukic@theiconic.com.au>
2024-11-09 14:48:19 +1100

    Add typing animation

--- src/app/components/letter-container.tsx
+++ src/app/components/letter-container.tsx
@@ -1,14 +1,33 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from "./letter-container.module.css";
 
 export default function LetterContainer({letter, backgroundColor}: {
   letter?: string,
   backgroundColor?: string
 }) {
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  const triggerAnimation = () => {
+    setIsAnimating(false);
+    setTimeout(() => setIsAnimating(true), 10);
+  };
+
+  useEffect(() => {
+    if (letter) {
+      triggerAnimation();
+    }
+  }, [letter]);
+
   return (
-    <div className={styles.letterContainerBox} style={{backgroundColor: backgroundColor}}>
+    <div
+      className={
+        `${styles.letterContainerBox}
+        ${isAnimating ? styles.cellFillAnimation : ''}`
+      }
+      style={{backgroundColor: backgroundColor}}
+    >
       {letter?.toUpperCase()}
     </div>
   );
