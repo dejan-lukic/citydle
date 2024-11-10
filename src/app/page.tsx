03f29ae584fbc983b7a4150d299467d8eba2fa8d
dejan-lukic <dejan.lukic@theiconic.com.au>
2024-11-10 14:02:23 +1100

    Clean up home page, make entire card clickable and add a container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { Card, CardMedia, CardContent, CardActions, Typography, Button, Grid2 as Grid } from '@mui/material';
+import { Card, CardMedia, CardContent, Typography, Grid2 as Grid, CardActionArea, Container } from '@mui/material';
 import Link from 'next/link';
 
 const games = [
@@ -19,35 +19,38 @@ export default function HomePage() {
       <Typography variant="h3" align="center" gutterBottom>
         Fun Friday Games
       </Typography>
-      <Grid container spacing={4}>
-        {games.map((game, index) => (
-          <Grid size={{ xs: 12, sm: 6, md: 4 }} key={index}>
-            <Card>
-              <CardMedia
-                component="img"
-                height="140"
-                image={game.image}
-                alt={`${game.title} Thumbnail`}
-              />
-              <CardContent>
-                <Typography variant="h5" component="div">
-                  {game.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {game.description}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Link href={game.link} passHref>
-                  <Button size="small" color="primary">
-                    Play Now
-                  </Button>
-                </Link>
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+
+      <Container maxWidth="md">
+        <Grid container spacing={4}>
+          {games.map((game, index) => (
+            <Grid size={{ xs: 12, sm: 6, md: 4 }} key={index}>
+              <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                <CardActionArea
+                  component={Link}
+                  href={game.link}
+                  prefetch
+                  sx={{ height: '100%' }}
+                >
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={game.image}
+                    alt={`${game.title} Thumbnail`}
+                  />
+                  <CardContent>
+                    <Typography variant="h5" component="div">
+                      {game.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {game.description}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </div>
   );
 }
