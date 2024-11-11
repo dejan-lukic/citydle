78d0f8961135092acda5fcba660a01ab7464af5f
dejan-lukic <dejan.lukic@theiconic.com.au>
2024-11-11 20:03:55 +1100

    Add animated squares

--- src/app/worldle/components/worldle-guess-row.tsx
+++ src/app/worldle/components/worldle-guess-row.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./worldle-guess-row.module.css";
 import { Guess } from "@/app/worldle/types/game-state";
 import { Stack } from "@mui/system";
@@ -8,12 +8,67 @@ interface WorldleGuessRowProps {
   guess?: Guess;
 }
 
+function generateSquareCharacters(accuracy: number): string[] {
+  const characters = new Array<string>(5);
+  const greenSquareCount = Math.floor(accuracy / 20);
+  const yellowSquareCount = accuracy - greenSquareCount * 20 >= 10 ? 1 : 0;
+  characters.fill("🟩", 0, greenSquareCount);
+  characters.fill("🟨", greenSquareCount, greenSquareCount + yellowSquareCount);
+  characters.fill("⬜", greenSquareCount + yellowSquareCount);
+
+  return characters;
+}
+
+const SQUARE_ANIMATION_LENGTH = 150;
+
 const WorldleGuessRow: React.FC<WorldleGuessRowProps> = (
   { guess }
 ) => {
+  const [animating, setAnimating] = React.useState(false);
+
+  useEffect(() => {
+    if (!guess) {
+      return;
+    }
+
+    setAnimating(true);
+
+    setTimeout(() => {
+      setAnimating(false);
+    }, SQUARE_ANIMATION_LENGTH * 6);
+  }, [guess]);
+
   if (!guess) {
     return (
       <div className={styles.worldleEmptyGuessRow}></div>
+    );
+  }
+
+  if (animating) {
+    const squares = generateSquareCharacters(guess.accuracy);
+
+    return (
+      <div
+        style={{display: "flex"}}
+      >
+        <div
+          className={styles.worldleSquareRow}
+        >
+          {squares.map((square, index) => (
+              <div
+                key={index}
+                className={styles.worldleAnimatedSquare}
+                style={{animationDelay: `${SQUARE_ANIMATION_LENGTH * index}ms`}}
+              >
+                {square}
+            </div>
+          ))}
+        </div>
+
+        <Typography className={styles.worldleAnimatedAccuracy}>
+          {guess.accuracy}%
+        </Typography>
+      </div>
     );
   }
 
@@ -31,11 +86,11 @@ const WorldleGuessRow: React.FC<WorldleGuessRowProps> = (
         {Math.round(Math.floor(guess.distanceToCorrectCountry))} km
       </Typography>
 
-      <Typography sx={{flex: 1}}>
+      <Typography sx={{flex: 0.5}}>
         {guess.directionEmoji}
       </Typography>
 
-      <Typography sx={{flex: 1}}>
+      <Typography sx={{flex: 0.5}}>
         {guess.accuracy}%
       </Typography>
     </Stack>
